e2c3999a0ab353a77c76b680d6bbf72ba9a84fc8
zhangrenchao <zhangrenchao@newrank.cn>
2022-09-27 21:47:59 +0800

    第一次提交:项目初始化

---
+++ src/test/java/bean/UserService.java
@@ -0,0 +1,28 @@
+package bean;
+
+import org.junit.Test;
+import springframework.BeanDefinition;
+import springframework.BeanFactory;
+
+public class UserService {
+
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+
+
+    @Test
+    public void test_BeanFactory(){
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注册 bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取 bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+
+}
